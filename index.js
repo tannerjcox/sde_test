0fefeda6059ee718d247e7f05f477307275773c0
Tanner <tanner@wavesandwebs.com>
2022-10-12 14:01:27 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const path = require('path');
+const readline = require('readline');
+
+const {
+    openFile,
+    processDriverFile,
+    assignDriversToRoutes,
+} = require('./lib');
+
+program
+    .version('1.0.0')
+    .name('driver-routes')
+    .description('Tool to assign drivers to routes')
+    .parse(process.argv);
+
+const main = async () => {
+    try {
+        const srcPath = path.join(process.cwd(), 'files');
+
+        const driverFileInterface = readline.createInterface({
+            input: openFile(path.join(srcPath, 'drivers.txt')),
+            crlfDelay: Infinity
+        });
+
+        let drivers = await processDriverFile(driverFileInterface);
+
+        const routeFileInterface = readline.createInterface({
+            input: openFile(path.join(srcPath, 'routes.txt')),
+            crlfDelay: Infinity
+        });
+
+        let assignments = await assignDriversToRoutes(routeFileInterface, drivers);
+
+        console.log('Routes assigned successfully!', assignments, assignments.length);
+    } catch (error) {
+        console.log('Error assigning routes.', error);
+    }
+};
+
+main();
+
